4bac83b2c75e6d948c88476518cfac981172be23
akmesebetul <65555591+akmesebetul@users.noreply.github.com>
2020-12-16 20:26:38 +0300

    Projenin Eski Dosyaları

---
+++ src/java/Util/DBConnection.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Util;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Casper
+ */
+public class DBConnection {
+
+    public static Connection getConnection() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public static void close(Connection con) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+   public Connection connect(){
+    Connection c = null ;
+    try{
+    Class.forName("com.mysql.jdbc.Driver").newInstance();
+     c = DriverManager.getConnection("jdbc:mysql://localhost:3306/hastane?user=root&password=1905");
+    }catch(SQLException e){
+            System.out.println(e.getMessage()) ;
+    
+    }   catch (ClassNotFoundException|InstantiationException|IllegalAccessException ex) {
+           
+        System.out.println(ex.getMessage())    ; 
+    } 
+    return c ;
+    
+    
+    }   
+}
+
+
---
+++ src/java/converter/doktorconverter.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package converter;
+
+import dao.doktorDAO;
+import entity.doktor;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+/**
+ *
+ * @author Casper
+ */
+@FacesConverter(value = "doktorconverter") //xhtml tarafında görünür olması için
+public class doktorconverter implements Converter {
+
+    private doktorDAO doktorDAO;
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) { //xhtml tarafından string olarak  gelen veriyi nesneye dönüştüren kod
+        return this.getDoktorDAO().find(Long.valueOf(value));
+    }
+
+    @Override
+    public String getAsString(FacesContext arg0, UIComponent arg1, Object arg2) { //nesneyi string return eder
+        doktor d = (doktor) arg2;
+        return d.getId_dok().toString();
+    }
+
+    public doktorDAO getDoktorDAO() {
+        if (this.doktorDAO == null) {
+            this.doktorDAO = new doktorDAO();
+        }
+        return doktorDAO;
+    }
+}
